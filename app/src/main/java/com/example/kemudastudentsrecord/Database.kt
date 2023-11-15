4dce5b4fc4dc1ac6c3326955eec02bff53020c00
Abdul Azim Bin Abdullah 4616 <4616@laksamanacollege.edu.bn>
2023-11-15 11:16:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/kemudastudentsrecord/Database.kt
@@ -0,0 +1,11 @@
+package com.example.kemudastudentsrecord
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Database : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_database)
+    }
+}
